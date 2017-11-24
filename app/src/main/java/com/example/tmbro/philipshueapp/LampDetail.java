ae73011f2431d4acdf07991c863663e80d34f41d
Thomas Brouwers <tmbrouwers@gmail.com>
2017-11-24 13:56:08 +0100

    nu wel goed gemerged hopelijk werkt het

--- app/src/main/java/com/example/tmbro/philipshueapp/LampDetail.java
+++ app/src/main/java/com/example/tmbro/philipshueapp/LampDetail.java
@@ -1,7 +1,10 @@
 package com.example.tmbro.philipshueapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class LampDetail extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class LampDetail extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lamp_detail);
+
+        Intent intent = getIntent();
+        HUELamp item = (HUELamp) intent.getSerializableExtra("LAMP_ITEM");
+
+        TextView name = (TextView) findViewById(R.id.nameText);
+        name.setText(item.getName());
+
+        SeekBar bri = (SeekBar) findViewById(R.id.briBar);
+        //bri.set
     }
 }
