0d569efa721cd3b97667431dda73baac28983155
Thomas Brouwers <tmbrouwers@gmail.com>
2017-11-24 11:58:31 +0100

    super legit merge

--- app/src/main/java/com/example/tmbro/philipshueapp/HUELamp.java
+++ app/src/main/java/com/example/tmbro/philipshueapp/HUELamp.java
@@ -14,13 +14,65 @@ import java.util.List;
 
 public class HUELamp implements Serializable {
 
-private JSONObject state;
-
-    public HUELamp() {
-    }
+    private JSONObject state;
+    private boolean on;
+    private int bri;
+    private int hue;
+    private int sat;
+    private String effect;
+    private String type;
+    private String name;
 
     public HUELamp(JSONObject json) {
-state = json.getJSONObject("state");
+
+        try {
+
+            state = json.getJSONObject("state");
+            on = state.getBoolean("on");
+            bri = state.getInt("bri");
+            hue = state.getInt("hue");
+            sat = state.getInt("sat");
+            effect = state.getString("effect");
+
+            type = json.getString("type");
+            name = json.getString("name");
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
 
     }
+
+    public JSONObject getState() {
+        return state;
+    }
+
+    public boolean isOn() {
+        return on;
+    }
+
+    public int getBri() {
+        return bri;
+    }
+
+    public int getHue() {
+        return hue;
+    }
+
+    public int getSat() {
+        return sat;
+    }
+
+    public String getEffect() {
+        return effect;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
+
