517f8361306d77f8c6d920521abcb59759ca988f
Unknown <tkrijgsm@avans.nl>
2017-11-24 11:55:59 +0100

    Added stuff and things
    huehuehueeuhheuehuehuehueheuhue

--- app/src/main/java/com/example/tmbro/philipshueapp/HUELamp.java
+++ app/src/main/java/com/example/tmbro/philipshueapp/HUELamp.java
@@ -14,19 +14,64 @@ import java.util.List;
 
 public class HUELamp implements Serializable {
 
-
-
-    public HUELamp() {
-    }
+    private JSONObject state;
+    private boolean on;
+    private int bri;
+    private int hue;
+    private int sat;
+    private String effect;
+    private String type;
+    private String name;
 
     public HUELamp(JSONObject json) {
 
         try {
 
+        state = json.getJSONObject("state");
+        on = state.getBoolean("on");
+        bri = state.getInt("bri");
+        hue = state.getInt("hue");
+        sat = state.getInt("sat");
+        effect = state.getString("effect");
+
+        type = json.getString("type");
+        name = json.getString("name");
 
         } catch (JSONException e) {
             e.printStackTrace();
         }
 
+    }
+
+    public JSONObject getState() {
+        return state;
+    }
+
+    public boolean isOn() {
+        return on;
+    }
+
+    public int getBri() {
+        return bri;
+    }
+
+    public int getHue() {
+        return hue;
+    }
+
+    public int getSat() {
+        return sat;
+    }
+
+    public String getEffect() {
+        return effect;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getName() {
+        return name;
     }
 }
